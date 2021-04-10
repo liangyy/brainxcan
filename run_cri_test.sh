1e8bd5615f9e043bd6c006d9184052c169b6bba5
Yanyu Liang <yanyul@uchicago.edu>
2021-04-10 17:41:25 -0500

    Update run_cri_test.sh

--- run_cri_test.sh
+++ run_cri_test.sh
@@ -1,5 +1,6 @@
-# cd brainxcan repo
-# update 'brainxcan_path' to path to brainxcan repo
+BXCAN=path-to-brainxcan-repo
+OUTDIR=path-to-output-directory
+OUTPREFIX=prefix-of-output
 
 module load conda
 module load gcc/6.2.0
@@ -8,5 +9,7 @@ module load plink/1.90
 conda activate /gpfs/data/im-lab/nas40t2/yanyul/softwares/miniconda2/envs/ukb_idp
 SNMK=/gpfs/data/im-lab/nas40t2/yanyul/softwares/miniconda2/envs/snakemake/bin/snakemake
 
-$SNMK -s brainxcan/snmk/run.snmk --configfile config.cri_test.yaml -p SBrainXcanAndMR -j5
- 
+$SNMK -s $BXCAN/brainxcan/snmk/run.snmk --configfile $BXCAN/config.cri_test.yaml -p SBrainXcanAndMR -j5 --config \
+  outdir=$OUTDIR \
+  brainxcan_path=$BXCAN \    
+  prefix=$OUTPREFIX
