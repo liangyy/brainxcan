56a2a044bd65e661a00883066a9e5361ab36fc44
Yanyu Liang <yanyul@uchicago.edu>
2021-04-10 17:35:12 -0500

    run_cri_test.sh

--- brainxcan/mr/run_mr_vis.R
+++ brainxcan/mr/run_mr_vis.R
@@ -21,15 +21,17 @@ opt <- parse_args(opt_parser)
 # srcpath = Sys.getenv("R_SRC")
 # source(paste0(srcpath, '/', 'mr_helper.R'))
 library(dplyr)
+library(ggplot2)
 library(patchwork)
 options(stringsAsFactors = F)
+theme_set(theme_classic(base_size = 12))
 
 # logging config
 logging::basicConfig(level = opt$log_level)
 
 logging::loginfo('Loading MR RDS and BrainXcan 1-line result.')
 mr_res = readRDS(opt$mr_rds)
-bxcan = read.table(opt$bxcan, header = T, sep = '\t')
+bxcan = read.csv(opt$bxcan)
 
 logging::loginfo('Plotting.')
 p1 = mr_res$idp2pheno$data %>% ggplot() + geom_hline(yintercept = 0, color = 'gray') + 
@@ -37,13 +39,13 @@ p1 = mr_res$idp2pheno$data %>% ggplot() + geom_hline(yintercept = 0, color = 'gr
   geom_point(aes(x = beta.exposure, y = beta.outcome), alpha = 0.5) + 
   geom_errorbar(aes(x = beta.exposure, ymin = beta.outcome - 1.96 * se.outcome, ymax = beta.outcome + 1.96 * se.outcome), alpha = 0.5) +
   geom_errorbarh(aes(y = beta.outcome, xmin = beta.exposure - 1.96 * se.exposure, xmax = beta.exposure + 1.96 * se.exposure), alpha = 0.5) +
-  th + xlab('SNP estimated effect in IDP') + ylab('SNP estimated effect in phenotype') + ggtitle('MR: IDP -> Phenotype')
+  xlab('SNP estimated effect in IDP') + ylab('SNP estimated effect in phenotype') + ggtitle('MR: IDP -> Phenotype')
 p2 = mr_res$pheno2idp$data %>% ggplot() + geom_hline(yintercept = 0, color = 'gray') + 
   geom_vline(xintercept = 0, color = 'gray') + 
   geom_point(aes(x = beta.exposure, y = beta.outcome), alpha = 0.5) + 
   geom_errorbar(aes(x = beta.exposure, ymin = beta.outcome - 1.96 * se.outcome, ymax = beta.outcome + 1.96 * se.outcome), alpha = 0.5) +
   geom_errorbarh(aes(y = beta.outcome, xmin = beta.exposure - 1.96 * se.exposure, xmax = beta.exposure + 1.96 * se.exposure), alpha = 0.5) +
-  th + xlab('SNP estimated effect in phenotype') + ylab('SNP estimated effect in IDP') + ggtitle('MR: Phenotype -> IDP')
+  xlab('SNP estimated effect in phenotype') + ylab('SNP estimated effect in IDP') + ggtitle('MR: Phenotype -> IDP')
 ggsave(opt$output_plot, p1 + p2, height = 4, width = 8)
 
 logging::loginfo('Collecting summary of results.')
@@ -57,6 +59,7 @@ df_res = rbind(
   df_mr, 
   bxcan %>% select(direction, method, nsnp, b, pval)
 )
-write.table(df_mr, opt$output_table, sep = '\t', col = T, row = F, quo = F)
+write.table(df_res, opt$output_table, sep = '\t', col = T, row = F, quo = F)
 
 logging::loginfo('Done.')
+
